70d46b2b69db07e7a8f04c118ac6b8ceed3f3ef5
Justin Staubach <justin@staubach.us>
2019-01-25 21:08:51 -0500

    update deploy script

--- deploy.bat
+++ deploy.bat
@@ -1,33 +1,33 @@
-@echo off
-ECHO PUSH MASTER
+@ECHO off
+ECHO "PUSH MASTER"
 git push -v origin master:master
-ECHO GIT STATUS
+ECHO "GIT STATUS"
 git status
-ECHO CHECKOUT STAGING
+ECHO "CHECKOUT STAGING"
 git checkout staging
-ECHO GIT STATUS
+ECHO "GIT STATUS"
 git status
-ECHO MERGE MASTER INTO STAGING
+ECHO "MERGE MASTER INTO STAGING"
 git merge master
-ECHO GIT STATUS
+ECHO "GIT STATUS"
 git status
-ECHO PUSH STAGING
-push -v origin staging:staging
-ECHO GIT STATUS
+ECHO "PUSH STAGING"
+git push -v origin staging:staging
+ECHO "GIT STATUS"
 git status
-ECHO CHECKOUT PROD
+ECHO "CHECKOUT PROD"
 git checkout prod
-ECHO GIT STATUS
+ECHO "GIT STATUS"
 git status
-ECHO MERGE STAGING INTO PROD
+ECHO "MERGE STAGING INTO PROD"
 git merge staging
-ECHO GIT STATUS
+ECHO "GIT STATUS"
 git status
-ECHO PUSH PROD
+ECHO "PUSH PROD"
 git push -v origin prod:prod
-ECHO GIT STATUS
+ECHO "GIT STATUS"
 git status
-ECHO CHECKOUT MASTER
+ECHO "CHECKOUT MASTER"
 git checkout master
-ECHO GIT STATUS
+ECHO "GIT STATUS"
 git status
