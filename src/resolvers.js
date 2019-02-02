8ab6b70f42f6501c1088d72e152bd1a5c2af576f
Justin Staubach <justin@staubach.us>
2019-02-02 08:12:39 -0500

    add babel eslint dep; clean up linting errors.

--- src/resolvers.js
+++ src/resolvers.js
@@ -10,7 +10,7 @@ module.exports = {
       const launches = paginateResults({
         after,
         pageSize,
-        results: allLaunches,
+        results: allLaunches
       });
 
       return {
@@ -19,21 +19,18 @@ module.exports = {
         // if the cursor of the end of the paginated results is the same as the
         // last item in _all_ results, then there are no more results after this
         hasMore: launches.length
-          ? launches[launches.length - 1].cursor !==
-            allLaunches[allLaunches.length - 1].cursor
-          : false,
+          ? launches[launches.length - 1].cursor !== allLaunches[allLaunches.length - 1].cursor
+          : false
       };
     },
-    launch: (_, { id }, { dataSources }) =>
-      dataSources.launchAPI.getLaunchById({ launchId: id }),
-    me: async (_, __, { dataSources }) =>
-      dataSources.userAPI.findOrCreateUser(),
+    launch: (_, { id }, { dataSources }) => dataSources.launchAPI.getLaunchById({ launchId: id }),
+    me: async (_, __, { dataSources }) => dataSources.userAPI.findOrCreateUser()
   },
   Mutation: {
     bookTrips: async (_, { launchIds }, { dataSources }) => {
       const results = await dataSources.userAPI.bookTrips({ launchIds });
       const launches = await dataSources.launchAPI.getLaunchesByIds({
-        launchIds,
+        launchIds
       });
 
       return {
@@ -41,10 +38,8 @@ module.exports = {
         message:
           results.length === launchIds.length
             ? 'trips booked successfully'
-            : `the following launches couldn't be booked: ${launchIds.filter(
-                id => !results.includes(id),
-              )}`,
-        launches,
+            : `the following launches couldn't be booked: ${launchIds.filter(id => !results.includes(id))}`,
+        launches
       };
     },
     cancelTrip: async (_, { launchId }, { dataSources }) => {
@@ -53,32 +48,30 @@ module.exports = {
       if (!result)
         return {
           success: false,
-          message: 'failed to cancel trip',
+          message: 'failed to cancel trip'
         };
 
       const launch = await dataSources.launchAPI.getLaunchById({ launchId });
       return {
         success: true,
         message: 'trip cancelled',
-        launches: [launch],
+        launches: [launch]
       };
     },
     login: async (_, { email }, { dataSources }) => {
       const user = await dataSources.userAPI.findOrCreateUser({ email });
-      if (user) return new Buffer(email).toString('base64');
-    },
+      if (user) return Buffer.from(email).toString('base64');
+    }
   },
   Launch: {
     isBooked: async (launch, _, { dataSources }) =>
-      dataSources.userAPI.isBookedOnLaunch({ launchId: launch.id }),
+      dataSources.userAPI.isBookedOnLaunch({ launchId: launch.id })
   },
   Mission: {
     // make sure the default size is 'large' in case user doesn't specify
     missionPatch: (mission, { size } = { size: 'LARGE' }) => {
-      return size === 'SMALL'
-        ? mission.missionPatchSmall
-        : mission.missionPatchLarge;
-    },
+      return size === 'SMALL' ? mission.missionPatchSmall : mission.missionPatchLarge;
+    }
   },
   User: {
     trips: async (_, __, { dataSources }) => {
@@ -90,9 +83,9 @@ module.exports = {
       // look up those launches by their ids
       return (
         dataSources.launchAPI.getLaunchesByIds({
-          launchIds,
+          launchIds
         }) || []
       );
-    },
-  },
+    }
+  }
 };
